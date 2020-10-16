be971939bd7e3e6bae3b68fc37742aa9fa8a9631
Anshu Beniwal <anshubeniwal@qainfotech.com>
2020-10-16 16:43:08 +0530

    First Commit

---
+++ PDF_BOT.bat
@@ -0,0 +1,1 @@
+"C:\Users\anshubeniwal\AppData\Local\UiPath\app-20.4.3\UiRobot.exe" -file "C:\Users\anshubeniwal\Desktop\RPA_PDF3.1\RPA_PDF_XAMLS\FinalSubmission_1.1.xaml" --input "{'basePath' : 'C:\\Users\\anshubeniwal\\Desktop\\RPA_PDF3.1\\RPA_PDF_XAMLS\\'}"
